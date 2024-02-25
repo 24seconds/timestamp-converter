60462d4eacdff231014a269efbd4c8d2a705d28b
Young <24stableyoung@gmail.com>
2024-02-25 16:50:32 +0900

    refactor code by removing deprecated function call

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use clap::{arg, Command};
 
 const TIMESTAMP_NANO_LEN: usize = 19;
 const TIMESTAMP_SECOND_LEN: usize = 10;
+const TIMESTAMP_SEC_TO_NANO: i64 = 1_000_000_000;
 
 fn main() {
     let matches = Command::new("time-converter")
@@ -22,30 +23,24 @@ fn main() {
 // unix timestamp 1653405628XXXXXXXXX
 // RFC 3339 format string should be printed
 fn parse_number(n: &str) {
-
-    // Inferring the input using length will work until ~ 2285 year
-    let (secs, nsecs) = match n {
+    let nsec = match n {
         t if n.len() == TIMESTAMP_NANO_LEN => {
-            // TODO(young): handle error
-            let t = t.parse::<i64>().unwrap();
-            let secs = t / 1_000_000_000;
-            let nsecs = t - secs;
-
-            (secs, nsecs)
+            t.parse::<i64>().unwrap()
         },
         t if n.len() == TIMESTAMP_SECOND_LEN => {
             let t = t.parse::<i64>().unwrap();
 
-            (t, 0)
-        }
+            t * TIMESTAMP_SEC_TO_NANO
+        },
         _ => {
             println!("input is not length of {} or {}", TIMESTAMP_NANO_LEN, TIMESTAMP_SECOND_LEN);
             return;
         }
     };
 
-    let t_utc = Utc.timestamp(secs, nsecs as u32);
+    let t_utc = Utc.timestamp_nanos(nsec);
     let t_local: DateTime<Local> = t_utc.into();
+
 
     println!("{}", t_utc.to_rfc3339());
     println!("{}", t_local.to_rfc3339());   
